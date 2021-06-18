835a111f9d98cf0b0a8c10f0f130855332d8e85b
W.Carlos <wendcarlos18@hotmail.com>
2021-06-18 19:36:55 -0300

    page-orphanage.css e page-orphanage.js
    fiz o css do orphanage e fiz o arquivo page-orphanage.js

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,19 @@
+//create map
+const map = L.map('mapid').setView([-27.2244003,-49.650684], 15)
+
+// create and add titleLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map)
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create and add marker
+L
+.marker([-27.2244003,-49.650684], { icon })
+.addTo(map)
