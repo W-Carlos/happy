9d17715a0aaaa238cd74ee427428c123fc2a99dc
W.Carlos <wendcarlos18@hotmail.com>
2021-06-29 18:33:42 -0300

    instalei o handlebars js

--- src/server.js
+++ src/server.js
@@ -5,14 +5,13 @@ const path = require('path')
 //iniciando o express
 const server = express()
 server
-
-// utilizando os arquivos estaticos
-.use(express.static('public'))
-
-// criar uma rota 
-.get('/', (request, response) => {
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+    // utilizando os arquivos estaticos
+    .use(express.static('public'))
+    
+    // criar uma rota 
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+    })
 
 // ligar o servidor
 server.listen(5500)
