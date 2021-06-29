f9445971bb97c714d7732cd5f4d2212d412c1d2c
W.Carlos <wendcarlos18@hotmail.com>
2021-06-29 16:01:14 -0300

    criei as rotas

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 // importar dependência
 const express = require('express')
+const path = require('path')
 
 //iniciando o express
 const server = express()
+server
+
+// utilizando os arquivos estaticos
+.use(express.static('public'))
 
 // criar uma rota 
-server.get('/', () => {
-    
+.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 // ligar o servidor
