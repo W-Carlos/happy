79131992b61b5934a3caf4db49e608d61cecb2fd
W.Carlos <wendcarlos18@hotmail.com>
2021-07-01 16:13:43 -0300

    exportando os modulos
    criei o arquivo fakedata
    exportando os modulos dos orfanatos

---
+++ src/database/fakedata.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.2244003",
+        lng: "-49.650684",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1599663254500-47f502ca2732?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTY2NDQy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+            "https://images.unsplash.com/photo-1599231091952-5274031ead92?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTY2NDE4&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ],
+
+
+    },
+    {
+        id: 2,
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+    }
+]
