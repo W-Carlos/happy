02583bddf95c88651fed0d3e0e00f75e012f8f64
W.Carlos <wendcarlos18@hotmail.com>
2021-07-01 17:29:07 -0300

    addMarker
    Para cada função invocando o span e o addMarker

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -13,16 +13,38 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-// create popup overlay
-const popup = L.popup({
-    closeButton: false, 
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>')
 
-// create and add marker
-L
-.marker([-27.2244003,-49.650684], { icon })
-.addTo(map)
-.bindPopup(popup)
+function addMarker({id, name, lat, lng}) {
+
+    // create popup overlay
+    const popup = L.popup({
+        closeButton: false, 
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`
+   ${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"></a>`)
+
+    // create and add marker
+    L
+    .marker([lat,lng], { icon })
+    .addTo(map)
+    .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+})
+
+
+
+
