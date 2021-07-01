70f7993822717c058b7adf9e5d26961736167ad0
W.Carlos <wendcarlos18@hotmail.com>
2021-07-01 16:19:01 -0300

    dois orfanatos cadastrados no fakedata

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -11,12 +11,24 @@ module.exports = [
 
             "https://images.unsplash.com/photo-1599231091952-5274031ead92?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTY2NDE4&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
         ],
-
-
+        instructions: "Veja como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas Das 18h até 8h",
+        open_on_weekends: "1"
     },
     {
         id: 2,
+        lat: "-27.2244003",
+        lng: "-49.640684",
         name: "Lar dos meninos",
         description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1599663254500-47f502ca2732?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTY2NDQy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+            "https://images.unsplash.com/photo-1599231091952-5274031ead92?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTY2NDE4&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ],
+        instructions: "Veja como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas Das 18h até 8h",
+        open_on_weekends: "0"
     }
 ]
