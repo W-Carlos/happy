1e3d3a72e39227c09e5728161830cf637cf5e296
W.Carlos <wendcarlos18@hotmail.com>
2021-07-02 19:51:24 -0300

    alterações no mapa

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,8 +6,12 @@ const opitions = {
     zoomControl: false
 }
 
+// get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat
+const lng = document.querySelector('span[data-lng]').dataset.lng
+
 //create map
-const map = L.map('mapid', opitions).setView([-27.2244003,-49.650684], 15)
+const map = L.map('mapid', opitions).setView([lat, lng], 15)
 
 // create and add titleLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
@@ -23,7 +27,7 @@ const icon = L.icon({
 
 // create and add marker
 L
-.marker([-27.2244003,-49.650684], { icon })
+.marker([lat, lng], { icon })
 .addTo(map)
 
 /* image gallery */
