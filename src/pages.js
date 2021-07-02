ff53f5762525ad5ca4cbc708274f4e384b5b4651
W.Carlos <wendcarlos18@hotmail.com>
2021-07-02 19:18:32 -0300

    imagens do orphanage

--- src/pages.js
+++ src/pages.js
@@ -6,8 +6,29 @@ module.exports = {
          return res.render('index')    
     },
 
-    orphanage(req, res) {
-        return res.render('orphanage')
+   async orphanage(req, res) {
+
+        const id = req.query.id
+
+        try {
+            const db = await Database
+            const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+            const orphanage = results[0]
+
+            orphanage.images = orphanage.images.split(",")
+            orphanage.firstImage = orphanage.images[0]
+
+            if(orphanage.open_on_weekends == "0") {
+                orphanage.open_on_weekends = false
+            } else {
+                orphanage.open_on_weekends = true
+            }
+            
+            return res.render('orphanage', { orphanage })
+        } catch (error) {
+            console.log(error)
+            return res.send('Erro no baco de dados!')
+        }
     },
 
     async orphanages(req, res) {
@@ -17,7 +38,7 @@ module.exports = {
             return res.render('orphanages', { orphanages })
         } catch (error) {
             console.log(error)
-            return res.send('Erro no banco de dados')
+            return res.send('Erro no banco de dados!')
         }    
     },
 
