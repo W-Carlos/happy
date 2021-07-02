66b2b729818c315538910b376c514ef34a9d6e5b
W.Carlos <wendcarlos18@hotmail.com>
2021-07-02 16:00:59 -0300

    finalizei o test.js

--- src/database/test.js
+++ src/database/test.js
@@ -1,35 +1,28 @@
 const Database = require('./db')
+const saveOrphanage = require('./saveOrphanage')
 
 Database.then(async db => {
     // inserir dados na tabela
-    await db.run(`
-        INSERT INTO orphanages (
-            lat,
-            lng,
-            name,
-            about,
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        ) VALUES (
-            "-27.2244003",
-            "-49.660684",
-            "Lar dos meninos",
-            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
-            "77988885555",
+    await saveOrphanage(db, {
+        lat: "-27.2244003",
+        lng: "-49.660684",
+        name: "Lar de acolhimento",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+        images: [
             "https://images.unsplash.com/photo-1599663254500-47f502ca2732?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTY2NDQy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
-            "Veja como se sentir a vontade e traga muito amor e paciência para dar.",
-            "Horários de visitas Das 18h até 8h",
-            "0"
-        );
-    `)
+
+            "https://images.unsplash.com/photo-1599231091952-5274031ead92?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTY2NDE4&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ].toString(),
+        instructions: "Veja como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    })
 
     // consultar dados na tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+    console.log(selectedOrphanages)
     
     //consultar somente um orphanage pelo id
-    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "1"')
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
     console.log(orphanage)   
 })
